0717dbc231b1196ecd98c158cff8edc3a7904427
Troyzhen <tropicboytroy@gmail.com>
2026-03-05 02:53:36 -0500

    Add README.md and LICENSE.txt.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <string>
+#include <wrl.h>
+#include <iostream>
+#include "d3dx12.h"
+
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
